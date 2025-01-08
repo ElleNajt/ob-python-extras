7107abc9370b858c53d418675223a05364696bcc
Elle Najt <lnajt4@gmail.com>
2025-01-07 23:05:06 -0500

    Add golden tests

---
+++ tests/golden/shell_with_tabulate.nix
@@ -0,0 +1,6 @@
+{ pkgs ? import <nixpkgs> { } }:
+
+let
+  pythonEnv = pkgs.python3.withPackages
+    (ps: with ps; [ pandas numpy scikit-learn matplotlib seaborn tabulate ]);
+in pkgs.mkShell { buildInputs = [ pythonEnv pkgs.pyright ]; }
