4efc1131c6f88d9ddecfbeae8db3bec600e160a9
Elle Najt <lnajt4@gmail.com>
2025-03-10 01:14:06 -0400

    Add dataframe_image_default.nix

--- tests/shell.nix
+++ tests/shell.nix
@@ -1,19 +1,14 @@
 { pkgs ? import <nixpkgs> {
+
   overlays = [
-
     (final: prev: {
-
       python3 = prev.python3.override {
         packageOverrides = pfinal: pprev: {
-          dataframe_image = final.python3Packages.callPackage
-            /home/elle/code/nixpkgs/pkgs/development/python-modules/dataframe_image/default.nix
-            { };
+          dataframe_image =
+            final.python3Packages.callPackage ./dataframe_image_default.nix { };
         };
       };
-    }
-
-    )
-
+    })
   ];
 
 } }:
