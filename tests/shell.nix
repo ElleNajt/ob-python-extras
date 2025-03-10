a9387655f7bbb79f88cfa4cc90bbad3b975b531c
Elle Najt <lnajt4@gmail.com>
2025-03-09 23:52:53 -0400

    Put nix shell a regular file

--- tests/shell.nix
+++ tests/shell.nix
@@ -1,1 +1,40 @@
-/home/elle/code/ob-python-extras/shell.nix
+{ pkgs ? import <nixpkgs> {
+  overlays = [
+
+    (final: prev: {
+
+      python3 = prev.python3.override {
+        packageOverrides = pfinal: pprev: {
+          dataframe_image = final.python3Packages.callPackage
+            /home/elle/code/nixpkgs/pkgs/development/python-modules/dataframe_image/default.nix
+            { };
+        };
+      };
+    }
+
+    )
+
+  ];
+
+} }:
+
+let
+  pythonEnv = pkgs.python3.withPackages (ps:
+    with ps; [
+      pandas
+      numpy
+      scikit-learn
+      matplotlib
+      seaborn
+      polars
+      ipdb
+      rich
+      pyarrow
+      pyspark
+      dataframe_image
+      # jinja2
+      plotly
+      # python3Packages.better-exceptions # or better-exceptions
+      ipython
+    ]);
+in pkgs.mkShell { buildInputs = [ pythonEnv pkgs.pyright ]; }
